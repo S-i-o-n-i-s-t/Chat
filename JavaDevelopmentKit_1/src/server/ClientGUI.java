54645a3daf9263c6d37aed7cecdd158782960399
S-i-o-n-i-s-t <metalljan@yandex.ru>
2023-10-06 18:20:51 +0300

    JavaDevelopmentKit_1

---
+++ JavaDevelopmentKit_1/src/server/ClientGUI.java
@@ -0,0 +1,127 @@
+package server;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ClientGUI extends JFrame {
+    private final int WIDTH = 350;
+    private final  int HEIGHT = 350;
+    private final int LOC_WIDTH = 750;
+    private final int LOC_HEIGHT = 400;
+
+    private String textMassage;
+    private String name;
+    private boolean connectClientGUI;
+
+    public void startClientGUI() {
+        this.connectClientGUI = true;
+        panelGroupRegistr.setVisible(false);
+    }
+    public void stopClientGUI() {
+        this.connectClientGUI = false;
+        panelGroupRegistr.setVisible(true);
+    }
+
+    // @Override
+    public String getName() {
+        return name;
+    }
+
+   // @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTextMassage() {
+        return textMassage;
+    }
+
+    public void setTextMassage(String textMassage) {
+        this.textMassage = textMassage;
+    }
+
+    JButton login, send;
+    JTextField port, ip, log, message;
+    JPasswordField password;
+    JPanel panelRegistr1, panelRegistr2, panelGroupRegistr, panelMessage;
+    JTextArea hustoryMessage;
+    public void setHustoryMessage(String textMassage){
+        hustoryMessage.setText(textMassage);
+    }
+    ClientGUI(ServerWindow serverWindow){
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocation(LOC_WIDTH, LOC_HEIGHT);
+        setTitle("ClientGUI");
+        setResizable(false);
+        clientGUIconnectPanel();
+        getText(send, message, serverWindow, this);
+        connectServer(login, serverWindow,this);
+
+        setVisible(true);
+    }
+    public void getText(JButton sendMessage, JTextField textMessage, ServerWindow serverWindow, ClientGUI clientGUI){
+        sendMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (connectClientGUI){
+                    setName(log.getText()); // Берем имя отправителя
+                    setTextMassage(getName() + "\n" + textMessage.getText() + "\n"); // Создаем текст сообщения
+                    serverWindow.setTextArea(getTextMassage(), clientGUI); // Отправляем сообщение на сервер
+                }
+                else clientGUI.setHustoryMessage("Вы не 'авторизированы'");
+            }
+        });
+    }
+    public void connectServer(JButton login, ServerWindow serverWindow, ClientGUI client){
+        login.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (serverWindow.statusServer()){
+                    serverWindow.addListClientGUI(client);
+                    client.setHustoryMessage(serverWindow.getText());
+                }
+                else {
+                    client.setHustoryMessage("Сервер отключен");
+                }
+            }
+        });
+    }
+
+    public void clientGUIconnectPanel(){
+        login = new JButton("login");
+        ip = new JTextField("192.10.1");
+        port = new JTextField("80.80.80");
+        log = new JTextField("Ivan Ivanov");
+        password = new JPasswordField("12345");
+
+        send = new JButton("send");
+        message = new JTextField();
+
+        panelRegistr1 = new JPanel(new GridLayout(1,3));
+        panelRegistr2 = new JPanel(new GridLayout(1,3));
+        panelGroupRegistr = new JPanel(new GridLayout(2,1));
+        panelRegistr1.add(ip);
+        panelRegistr1.add(port);
+        panelRegistr2.add(log);
+        panelRegistr2.add(password);
+        panelRegistr2.add(login);
+        panelGroupRegistr.add(panelRegistr1);
+        panelGroupRegistr.add(panelRegistr2);
+
+        panelMessage = new JPanel(new GridLayout(1, 2));
+        panelMessage.add(message);
+        panelMessage.add(send);
+        //getText(send, message, serverWindow);
+
+        hustoryMessage = new JTextArea();
+        hustoryMessage.setLineWrap(true);
+        JScrollPane jScrollPane = new JScrollPane(hustoryMessage);
+
+        add(jScrollPane);
+        add(panelGroupRegistr, BorderLayout.NORTH);
+        add(panelMessage, BorderLayout.SOUTH);
+    }
+}
---
+++ JavaDevelopmentKit_1/src/server/ServerWindow.java
@@ -0,0 +1,170 @@
+package server;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ServerWindow extends JFrame {
+    private final int WIDTH = 450;
+    private final int HEIGHT = 450;
+    private boolean switchServer;
+    private StringBuilder chat = new StringBuilder();
+    private String husroryMassage;
+    private String EDFRWE = "Сервер Прекратил работу";
+    private List<ClientGUI> clientGUIList;
+    private File file = new File("temp.txt");
+    JPanel jPanelButton;
+    JButton start, stop;
+    JTextArea text;
+    public void setChat(String massage){
+        this.chat.append(massage);
+    }
+    public String getHusroryMassage(){
+        return this.husroryMassage;
+    }
+    public void setHusroryMassage(StringBuilder text){
+        husroryMassage = new String(text);
+    }
+    public boolean getSwitchServer(){
+        return switchServer;
+    }
+    public boolean statusServer(){
+        return switchServer;
+    }
+    public String getText(){
+        return text.getText();
+    }
+    /**
+     * Метод запуска сервера
+     */
+    public void startServer(){
+        chat = new StringBuilder();
+        getTextFile();
+        switchServer = true;
+    }
+    /**
+     * Метод остановки сервера
+     */
+    public void stopServer(){
+        switchServer = false;
+        setTextFile(getHusroryMassage());
+        for (int item = 0; item < clientGUIList.size(); item++){
+            clientGUIList.get(item).stopClientGUI();
+            clientGUIList.get(item).setHustoryMessage("Сервер Прекратил работу");
+        }
+        text.setText("Сервер Прекратил работу");
+    }
+    /**
+     * Метод для добавления в массив новых клиентов
+     * @param clientGUI
+     */
+    public void addListClientGUI(ClientGUI clientGUI){
+        clientGUI.startClientGUI();
+        clientGUIList.add(clientGUI);
+    }
+    /**
+     * Метод объединяющий добавления "истории сообщений" в окно сервера и подключающихся пользователей
+     * @param textMassage "Входящее сообщение"
+     * @param clientGUI "Отправитель"
+     */
+    public void setTextArea(String textMassage, ClientGUI clientGUI){
+        setChat(textMassage); // Добавляем сообщение к нашей истории сообщений
+        setHusroryMassage(chat); // Создаем строковую переменную "история сообщений"
+        text.setText(getHusroryMassage()); // Добавляем историю сообщений на "панель"textMassage
+        sendingMassage(getHusroryMassage(), clientGUI); // Передаем историю сообщений всем подключенным пользователям
+    }
+
+    /**
+     * Метод для отправки сообщений пользователям
+     * @param massage входящее сообщение
+     * @param clientGUI пользователь
+     */
+    public void sendingMassage(String massage, ClientGUI clientGUI){
+        if (!getSwitchServer()){
+            clientGUI.setHustoryMessage("Сервер отключен"); // Сообщаем пользователю, что сервер отключен
+        }
+        else {
+            for (int item = 0; item < clientGUIList.size(); item++){
+                clientGUIList.get(item).setHustoryMessage(massage); // Отправляем пользователям историю сообщений
+            }
+        }
+    }
+
+    /**
+     * Чтение данных из файла
+     */
+    public void getTextFile(){
+        try {
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+            while (bufferedReader.ready()){
+                setChat(bufferedReader.readLine() + "\n");
+                setHusroryMassage(chat);
+                text.setText(getHusroryMassage());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * Метод добавления текста в файл
+     * @param text
+     */
+    public void setTextFile(String text){
+        try {
+            FileWriter fileWriter = new FileWriter(file);
+            fileWriter.write(text);
+            fileWriter.flush();
+            fileWriter.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void buttonStart(JButton startButton){
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                startServer();
+            }
+        });
+    }
+    public void buttonStop(JButton stopButton){
+        stopButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                stopServer();
+            }
+        });
+    }
+    public void ServerWindowPanel(){
+        text = new JTextArea(24,20);
+
+        text.setLineWrap(true);
+        JScrollPane jScrollPane = new JScrollPane(text);
+        start = new JButton("Start");
+        stop = new JButton("Stop");
+        buttonStart(start);
+        buttonStop(stop);
+        jPanelButton = new JPanel(new GridLayout(1,2));
+        jPanelButton.add(start);
+        jPanelButton.add(stop);
+
+        add(jPanelButton, BorderLayout.SOUTH);
+        add(jScrollPane, BorderLayout.NORTH);
+    }
+    ServerWindow(){
+        clientGUIList = new ArrayList<>();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setTitle("Server Window");
+        setResizable(true);
+        setLocationRelativeTo(null);
+        ServerWindowPanel();
+        setVisible(true);
+    }
+}
